fefcd0d9b409519ef5d4e619b2635fbcc702474c
Jordan Shaw <jordanshaw@Jordans-Macbook.local>
2016-04-13 10:47:47 -0400

    fixed csrf

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ MongoClient.connect(config.db, function(err, db) {
     }));
 
     // csrf defense
-    app.use(express.csrf());
+    app.use(csrf());
 
     app.use(function(req, res, next) { 
         res.locals.csrftoken = req.csrfToken(); 
